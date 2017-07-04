da4ae56bb0f4a997e486e6e503b1351bdc218113
sori-kang <sori.kang@rea-group.com>
2017-07-04 15:01:00 +1000

    Display contacs by using component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts;
+
+    return <ol>
+      {people.map(person => <li>{person.name}</li>)}
+      </ol>;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={[{ name: 'Tyler' }, { name: 'Sam' }] }/>
+        <ContactList contacts={[{ name: 'Claire' }, { name: 'Joe' }] }/>
+        <ContactList contacts={[{ name: 'Daniel' }, { name: 'Susan' }] }/>
       </div>
     );
   }
