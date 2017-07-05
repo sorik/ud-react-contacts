4e107bb177c09b762e809246740fcd6525b249ad
sori-kang <sori.kang@rea-group.com>
2017-07-05 21:29:30 +1000

    Add style and icon files for contact app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React, { Component } from 'react';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map(person => <li>{person.name}</li>)}
-      </ol>;
-  }
-}
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[{ name: 'Tyler' }, { name: 'Sam' }] }/>
-        <ContactList contacts={[{ name: 'Claire' }, { name: 'Joe' }] }/>
-        <ContactList contacts={[{ name: 'Daniel' }, { name: 'Susan' }] }/>
-      </div>
-    );
+    return <div>Hello World</div>
   }
 }
-
 export default App;
