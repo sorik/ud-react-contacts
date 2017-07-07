c6160fec675c6e3a35b89153e44bb7876b224258
sori-kang <sori.kang@rea-group.com>
2017-07-07 18:16:55 +1000

    Show contacts which matches the search query. - Refer this.state to get the source of truth for the search query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
   static propTypes ={
@@ -15,6 +17,19 @@ class ListContacts extends Component {
     this.setState({query: query.trim()})
   }
 
+  showingContacts() {
+    let contacts;
+
+    if(this.state.query) {
+      const matcher = new RegExp(escapeRegExp(this.state.query), 'i');
+      contacts = this.props.contacts.filter((contact) => matcher.test(contact.name));
+    } else {
+      contacts =  this.props.contacts;
+    }
+
+    return contacts.sort(sortBy('name'));
+  }
+
   render() {
     return (
       <div className='list-contacts'>
@@ -28,7 +43,7 @@ class ListContacts extends Component {
         />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map(contact => (
+          {this.showingContacts().map(contact => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
               <div className='contact-details'>
