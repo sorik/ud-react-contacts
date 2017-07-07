b046b328cf8f3eade0395628c781fe6653e7cd70
sori-kang <sori.kang@rea-group.com>
2017-07-07 16:48:30 +1000

    Change ListContacts to React Component class from stateless functional component. - ListContacts component will manage state which has search query.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ListContacts(props) {
-  return (
-    <ol className='contact-list'>
-      {props.contacts.map(contact => (
-        <li key={contact.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-          </div>
-          <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  )
-}
+class ListContacts extends React.Component {
+  static propTypes ={
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map(contact => (
+          <li key={contact.id} className='contact-list-item'>
+            <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
 }
 
 export default ListContacts;
