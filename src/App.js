5bb12b6a9c1176602750424b9abc7b2b716c8e65
sori-kang <sori.kang@rea-group.com>
2017-07-09 22:58:58 +1000

    Use componentDidMount function to fetch contacts data from server. - getting data from server by using lifecyle event. - setting state after getting data makes the component re-rendered.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   }
 
   removeContact = (contact) => {
@@ -31,8 +13,14 @@ class App extends Component {
     }))
   }
 
+  componentDidMount() {
+      ContactsAPI.getAll().then((contacts) => {
+        this.setState({ contacts })
+      })
+  }
+
   render() {
     return <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
   }
 }
-export default App;
+export default App
